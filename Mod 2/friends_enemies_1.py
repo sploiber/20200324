0dae47c76db8a7b1f5d612ccd498a7427f48a270
Joel M. Gottlieb <joel.gottlieb@gmail.com>
2020-03-04 13:58:38 -0500

    First version

---
+++ Mod 2/friends_enemies_1.py
@@ -0,0 +1,14 @@
+import dimod
+import dwave.inspector
+from dwave.system import DWaveSampler, EmbeddingComposite
+
+Q = {(0, 0): 1, (3, 3): 1, (0, 1): -2, (1, 2): 2, (2, 3): -2}
+
+chainstrength = 5
+numruns = 1000
+
+bqm = dimod.BinaryQuadraticModel.from_qubo(Q, offset=-2)
+print(bqm)
+sampler = EmbeddingComposite(DWaveSampler())
+sampleset = sampler.sample(bqm, chain_strength=chainstrength, num_reads=numruns)
+dwave.inspector.show(bqm, sampleset, sampler)
