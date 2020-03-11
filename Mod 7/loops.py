4d63960ee5b1940e9872daf9cdb33d0fefa3be30
Joel M. Gottlieb <joel.gottlieb@gmail.com>
2020-03-11 12:12:20 -0400

    First iteration

---
+++ Mod 7/loops.py
@@ -0,0 +1,13 @@
+import networkx as nx
+for i in range(10):
+    print(i)
+
+# Iterate through list - a bit more Pythonic
+z = [42, 15, 9, 'italy', 14]
+for z_item in z:
+    print(z_item)
+
+# iterate through a dict
+z2 = {42: "tyger", 15: "burning", 9: "bright", 3: "yeats"}
+for z_key in z2:
+    print(z_key, z2[z_key])
